f6c299102bb02e47b85751a2af03b6f116cfc265
FoxChrisRealTheGit <cfox1290@gmail.com>
2017-12-02 10:46:56 -0700

    Finished Assignment

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 
 
 class Sum extends Component {
- 
+ constructor(){
+     super();
+
+     this.state ={
+         number1 : 0,
+         number2 : 0,
+         sum: null
+
+     }
+ }
+
+ changeNumber1(val){
+     this.setState({number1: val})
+ }
+ changeNumber2(val){
+    this.setState({number2: val})
+ }
+
+ solveProblem(){
+     var num1 = Number(this.state.number1);
+     var num2 = Number(this.state.number2);
+
+     var answer = (num1 + num2);
+
+     this.setState({sum: answer})
+ }
     render() {
 
         return (
-            <p>Sum</p>
+            <div className="puzzleBox sumPB">
+                    <h4>Sum</h4>
+                    <input className="inputLine" onChange = {(e)=>this.changeNumber1(e.target.value)}></input>
+                    <input className="inputLine" onChange = {(e)=>this.changeNumber2(e.target.value)}></input>
+                    <button className="confirmationButton" onClick = {() => this.solveProblem()}>Add</button>
+                    <span className="resultsBox">Sum: {JSON.stringify(this.state.sum)}</span>
+
+                </div>
         )
     }
 }
